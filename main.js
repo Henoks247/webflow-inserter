da0c8def5a83b5f1f6cbd21eb9228cf8b74c723a
Henoks247 <67102412+Henoks247@users.noreply.github.com>
2023-07-04 18:59:00 +0300

    both

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ console.log("code link functionality check");
 
 console.log($(".extraheading").text());
 
-let x = 0;
+// For bedrooms
+
+let bedroomx = 0;
 // on button click
 $(".plusdiv1").on("click", () => {
   console.log("get success");
   // increment & set new value
-  $(".inputfield1.w-input").val(++x);
+  $(".inputfield1.w-input").val(++bedroomx);
 });
 
 $(".minusdiv1").on("click", () => {
   // decrement & set new value
   if (x > 0) {
-    $(".inputfield1.w-input").val(--x);
+    $(".inputfield1.w-input").val(--bedroomx);
   }
 });
 
@@ -31,6 +33,45 @@ $(".inputfield1.w-input").change(function () {
   // if it's a number
   if (num) {
     // assign its value to x
-    x = num;
+    bedroomx = num;
+  } else {
+    console.log("Not a number");
+  }
+});
+
+// For bathrooms
+
+let bathroomx = 0;
+// on button click
+$(".plusdiv2").on("click", () => {
+  console.log("get success");
+  // increment & set new value
+  $(".inputfield2.w-input").val(++bathroomx);
+});
+
+$(".minusdiv2").on("click", () => {
+  // decrement & set new value
+  if (x > 0) {
+    $(".inputfield2.w-input").val(--bathroomx);
+  }
+});
+
+/* This next part was added post-video recording added
+  to make sure the plus and minus work with a manual entry of
+  a number in the text input. If you type 5 into the input manually,
+  and then press (+), it will count to 6 instead of starting to
+  count at 1. Nice!!
+  */
+
+// on input value change
+$(".inputfield2.w-input").change(function () {
+  // convert input value to number
+  const num = Number($(this).val());
+  // if it's a number
+  if (num) {
+    // assign its value to x
+    bathroomx = num;
+  } else {
+    console.log("Not a number");
   }
 });
